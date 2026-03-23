9433f2336ea72e5d1478e0170613b877ca549482
idahjnr <ohigboche2022@gmail.com>
2026-03-23 22:32:34 +0100

    made correction to videoSection

--- react-app/src/components/VideoSection.jsx
+++ react-app/src/components/VideoSection.jsx
@@ -1,15 +1,20 @@
 function VideoSection() {
   return (
     <div className="video-section"> 
-        <h2>How Planetary Data Helps Us Understand Space</h2>
+      <h2>How Planetary Data Helps Us Understand Space</h2>
+
       <p className="subtitle">
-        Comparing <span>mass, diameter, gravity, </span> and <span> density </span> helps us understand how planets form and behave.
+        Comparing <span>mass, diameter, gravity, </span> and <span>density</span> helps us understand how planets form and behave.
       </p>
 
       <div className="video-card">
-        <video controls poster="https://images.unsplash.com/photo-1454789548928-9efd52dc4031"> <source src="planet.mp4" type="video/mp4" />
+        <video autoPlay muted loop controls>
+          <source 
+            src= "https://www.pexels.com/download/video/36029081/"
+            type="video/mp4" 
+          />
+          Your browser does not support the video tag.
         </video>
-
       </div>
     </div>
   );
