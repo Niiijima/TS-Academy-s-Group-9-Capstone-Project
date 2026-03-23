b6233a739afde9f8ad2ef2e01465ef75cc023ddf
Niiijima <qagwu11@gmail.com>
2026-03-23 21:07:20 +0100

    update footer with required links

--- react-app/src/components/Footer.jsx
+++ react-app/src/components/Footer.jsx
@@ -7,6 +7,18 @@ export default function Footer() {
       <div className="container footer-content">
         <p>© 2026 TS ACADEMY Group Capstone Project. All rights reserved</p>
         <div className="footer-links">
+                    <a href="https://github.com/Anurella" target="_blank" rel="noopener noreferrer">
+            Amaka-Github
+          </a>
+          <a href="https://www.linkedin.com/in/ifeomaokocha" target="_blank" rel="noopener noreferrer">
+            Ifeoma-LinkedIn
+          </a>
+          <a href="https://tsacademyonline.com/" target="_blank" rel="noopener noreferrer">
+            TS Academy
+          </a>
+          <a href="https://github.com/Niiijima/TS-Academy-s-Group-9-Capstone-Project.git" target="_blank" rel="noopener noreferrer">
+            Group 9 GitHub Repository
+          </a>
           <a href="#">Privacy</a>
           <a href="#">Terms</a>
         </div>
